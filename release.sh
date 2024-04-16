438aa38546d3dba865e6e6215f20761eda09e429
Phil Rzewski <philrzewski@gmail.com>
2024-04-16 17:37:37 +0000

    Enable ssl-log-ext (#13)

--- release.sh
+++ release.sh
@@ -73,7 +73,10 @@ $sudo pip3 install btest wheel
 install_zeek_package brimdata/geoip-conn c9dd7f0f8d40573189b2ed2bae9fad478743cfdf
 install_zeek_package salesforce/hassh 76a47abe9382109ce9ba530e7f1d7014a4a95209
 install_zeek_package salesforce/ja3 421dd4f3616b533e6971bb700289c6bb8355e707
-echo "@load policy/protocols/conn/community-id-logging" | $sudo tee -a /usr/local/zeek/share/zeek/site/local.zeek
+$sudo tee -a /usr/local/zeek/share/zeek/site/local.zeek << EOF
+@load policy/protocols/conn/community-id-logging
+@load policy/protocols/ssl/ssl-log-ext
+EOF
 
 # Work around https://github.com/zeek/zeek/issues/3534 on Windows
 [[ $(uname) =~ "NT" ]] &&
