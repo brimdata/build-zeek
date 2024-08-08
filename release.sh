6a6049268f43b53360abca4b33904ad15e6a5dee
Phil Rzewski <phil@brimdata.io>
2024-08-08 16:53:54 -0700

    Build from Zeek v7.0.0 and use latest GeoIP database

--- release.sh
+++ release.sh
@@ -70,7 +70,7 @@ install_zeek_package() {
 
 $sudo pip3 install btest wheel
 
-install_zeek_package brimdata/geoip-conn 4be3eb91a556c91659953f27495b0ab52dae9219
+install_zeek_package brimdata/geoip-conn aba34d6b9e87d7e21a895f2513d809b4dfcf804d
 install_zeek_package salesforce/hassh 76a47abe9382109ce9ba530e7f1d7014a4a95209
 install_zeek_package salesforce/ja3 421dd4f3616b533e6971bb700289c6bb8355e707
 
